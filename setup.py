d3d1dd795ec9b9e23f9909c48ea4baf787463b0d
Michael Weiss <code@mweiss.ch>
2023-09-29 11:45:55 +0000

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ from setuptools import find_packages, setup
 
 import license_utils
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(_ROOT, 'README.md')) as f:
@@ -20,8 +17,19 @@ setup(
     author="SPDX",
     url="https://github.com/miweiss/license-utils",
     packages=find_packages(exclude=['tests*']),
-    install_requires=requirements,
-    setup_requires=['setuptools>=39.0.1'],
+    install_requires=[
+        "aiohttp>=3.8.5",
+    ],
+    extras_require={
+        "test": [
+            "pytest",  
+        ],
+        "lint": [
+            "black==23.3.0",
+            "isort==5.12.0",
+            "docstr-coverage==2.2.0",
+        ],
+    },
     entry_points={'console_scripts': [
         'spdx-license-matcher = spdx_license_matcher.matcher:matcher']},
     keywords='spdx license license-matcher',
