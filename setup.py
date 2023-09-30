9f7d4ecffc2bc77f303e866c5e532ef7c6f55d46
Michael Weiss <code@mweiss.ch>
2023-09-30 13:15:53 +0000

    :recycle: single source of truth for version string

--- setup.py
+++ setup.py
@@ -4,24 +4,37 @@ from setuptools import find_packages, setup
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(_ROOT, 'README.md')) as f:
+with open(os.path.join(_ROOT, "README.md")) as f:
     LONG_DESCRIPTION = f.read()
+
+
+# Taken from: https://packaging.python.org/en/latest/guides/single-sourcing-package-version/#single-sourcing-the-package-version
+# TODO replace this once transitioned to pyproject.toml, as described in the above link
+def get_version(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, rel_path), "r") as fp:
+        init_file = fp.read()
+
+    for line in init_file.splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name="license-utils",
-    version="0.0.1",
+    version=get_version("license_utils/__init__.py"),
     description="Various utilities for working with SPDX / OSS licenses, including a spdx-based license matcher.",
     long_description=LONG_DESCRIPTION,
     author="SPDX",
     url="https://github.com/miweiss/license-utils",
-    packages=find_packages(exclude=['tests*']),
-    install_requires=[
-        "aiohttp>=3.8.5",
-        "requests>=2.31.0"
-    ],
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=["aiohttp>=3.8.5", "requests>=2.31.0"],
     extras_require={
         "test": [
-            "pytest",  
+            "pytest",
         ],
         "lint": [
             "black==23.3.0",
@@ -29,7 +42,10 @@ setup(
             "docstr-coverage==2.2.0",
         ],
     },
-    entry_points={'console_scripts': [
-        'spdx-license-matcher = spdx_license_matcher.matcher:matcher']},
-    keywords='spdx license license-matcher',
+    entry_points={
+        "console_scripts": [
+            "spdx-license-matcher = spdx_license_matcher.matcher:matcher"
+        ]
+    },
+    keywords="spdx license license-matcher",
 )
